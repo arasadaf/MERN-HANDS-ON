4bad01ebb472078ca17a09b1fc8a160f44d3bb35
arasadaf <arasadaf012@gmail.com>
2026-01-29 19:11:42 +0530

    Update product.js

--- ATP Sunteck/week2/e-commerce/product.js
+++ ATP Sunteck/week2/e-commerce/product.js
@@ -1,45 +1,40 @@
-// product.js
 const products = [
-  { id: 1, name: 'Laptop', price: 50000, stock: 10, category: 'electronics' },
-  { id: 2, name: 'Phone', price: 30000, stock: 15, category: 'electronics' },
-  { id: 3, name: 'Headphones', price: 2000, stock: 25, category: 'accessories' },
-  { id: 4, name: 'Mouse', price: 500, stock: 50, category: 'accessories' },
-  { id: 5, name: 'Keyboard', price: 1500, stock: 30, category: 'accessories' }
+  { id: 1, name: "Laptop", price: 50000, stock: 10, category: "electronics" },
+  { id: 2, name: "Phone", price: 30000, stock: 15, category: "electronics" },
+  { id: 3, name: "Headphones", price: 2000, stock: 25, category: "accessories" },
+  { id: 4, name: "Mouse", price: 500, stock: 50, category: "accessories" },
+  { id: 5, name: "Keyboard", price: 1500, stock: 30, category: "accessories" }
 ];
 
-function getProductById(id) {
+ // Find and return product by ID
+export function getProduct(id) {
   return products.find(p => p.id === id);
 }
 
-function getAllProducts() {
+// Return all products
+export function getAllProducts() {
   return products;
 }
 
-function getProductsByCategory(category) {
+// Filter products by category
+export function getProductsByCategory(category) {
   return products.filter(p => p.category === category);
 }
 
-function searchProducts(query) {
-  return products.filter(p =>
-    p.name.toLowerCase().includes(query.toLowerCase())
-  );
+ // Search products by name (case-insensitive)
+export function searchProducts(query) {
+  return products.filter(p =>p.name.toLowerCase().includes(query.toLowerCase()));
 }
 
-function checkStock(productId, quantity) {
-  let product = getProductById(productId);
+//Check if product has enough stock
+// Return true/false
+export function checkStock(pid, quantity) {
+    const product = getProduct(pid);
   return product && product.stock >= quantity;
 }
 
-function reduceStock(productId, quantity) {
-  let product = getProductById(productId);
+// Reduce product stock after purchase
+export function reduceStock(pid, quantity) {
+  const product = getProduct(pid);
   if (product) product.stock -= quantity;
 }
-
-export {
-  getProductById,
-  getAllProducts,
-  getProductsByCategory,
-  searchProducts,
-  checkStock,
-  reduceStock
-};
