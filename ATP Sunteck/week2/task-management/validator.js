e9301cb29b341d0bb767a791249614bde3b0ecda
arasadaf <arasadaf012@gmail.com>
2026-01-29 19:36:09 +0530

    Update validator.js

--- ATP Sunteck/week2/task-management/validator.js
+++ ATP Sunteck/week2/task-management/validator.js
@@ -1,28 +1,40 @@
-// validator.js
-
 // Validate task title (not empty, min 3 chars)
-function validateTitle(title) {
-  if (title && title.trim().length >= 3) {
+export function validateTitle(title) 
+{
+    if(title.length===0)
+    {
+        return "Title is empty"
+    }
+    if (title.length<3)
+    {
+        return "Title should have min of 3 characters"
+    }
     return true;
-  }
-  return false;
+}
+//Validate priority (must be: low, medium, high)
+export function validatePriority(priority)
+{
+   if (priority ==="low" || priority==="medium" || priority==="high")
+   {
+    return true
+   }
+   else
+   {
+    return "invalid priority"
+   }
 }
 
-// Validate priority (low, medium, high)
-function validatePriority(priority) {
-  let allowed = ['low', 'medium', 'high'];
-  return allowed.includes(priority);
+//Validate due date (must be future date)
+export function validateDueDate(date) 
+{
+    let today=new Date();
+    let d=new Date(date)
+    if (today<d)
+    {
+        return true
+    }
+    else
+    {
+        return "Due date is completed"
+    }
 }
-
-// Validate due date (future date)
-function validateDueDate(date) {
-  let today = new Date();
-  let dueDate = new Date(date);
-
-  if (dueDate > today) {
-    return true;
-  }
-  return false;
-}
-
-export { validateTitle, validatePriority, validateDueDate };
