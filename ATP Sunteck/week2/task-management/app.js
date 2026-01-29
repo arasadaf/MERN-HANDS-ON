975e815c008473862b134f99bc40fbb1e71cbee1
arasadaf <arasadaf012@gmail.com>
2026-01-29 19:34:59 +0530

    Update app.js

--- ATP Sunteck/week2/task-management/app.js
+++ ATP Sunteck/week2/task-management/app.js
@@ -1,11 +1,17 @@
-// app.js
-import { addTask, getAllTasks, completeTask } from './task.js';
+import { addTask, getAllTasks, completeTask } from "./task.js";
+ // TODO: Import task functions
+  // 1. Add some tasks
+console.log(addTask("Learn JS", "high", "2026-01-30"));
+console.log(addTask("Do HW", "medium", "2026-02-10"));
 
-console.log(addTask('Learn JS', 'high', '2026-02-01'));
-console.log(addTask('Practice coding', 'medium', '2026-02-05'));
+ // 2. Display all tasks
+console.log("All Tasks:");
+console.log(getAllTasks());
 
-console.log('All Tasks:', getAllTasks());
-
+// 3. Complete a task
+console.log("Completing Task 1:");
 console.log(completeTask(1));
 
-console.log('Updated Tasks:', getAllTasks());
+// 4. Display all tasks again
+console.log("All Tasks After Completion:");
+console.log(getAllTasks());
